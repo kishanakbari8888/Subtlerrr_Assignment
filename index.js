37d4872c863c949ac162ca8cb79dc3a7d7161b78
Kishan Akbari <88237282+kishanakbari8888@users.noreply.github.com>
2023-04-23 20:24:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -72,6 +72,50 @@ app.get('/v1/get_player_average', async (req, res) => {
     
 });
 
+app.get('/', async (req, res) => {
+    
+    connection.query('SELECT * FROM abcd', (error, result, fields) => {
+       
+        if (error) {
+            console.log(error);
+            res.send("Better Luck Next Time");
+        }
+        
+        
+        let scoreArray = [];
+    
+        for(var i=0; i<result.length; ++i)
+        {
+            let name = result[i].p_name;
+            let count = 0;
+            let sum = 0;
+            
+            delete result[i].p_name;
+            var keys = Object.keys( result[i] );
+            keys.sort( function ( a, b ) { 
+
+                let numa = a.substring(5);
+                let numb = b.substring(5);
+                return Stoi(numb)-Stoi(numa); 
+            });
+
+
+            for(var j = 0; j<5 && j<keys.length; ++j)
+            {
+                sum += result[i][keys[j]];
+                count++;
+            }
+        
+            averegescore = sum/count;
+            scoreArray.push({name,averegescore});
+            
+        }
+
+        return res.json(scoreArray);
+    });
+    
+});
+
 // Start the server
 app.listen(3000, (req,res) => {
   console.log('Server started on port 3000');
